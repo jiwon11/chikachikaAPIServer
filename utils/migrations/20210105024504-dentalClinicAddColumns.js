a21401522b43419b717384fdd85adb8db1a5e6fc
jiwon11 <jeewonre@naver.com>
2021-01-05 12:01:53 +0900

    치과병원 컬럼 추가-설명,우리동네 실명제

---
+++ utils/migrations/20210105024504-dentalClinicAddColumns.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn("dental_clinics", "description", { type: Sequelize.STRING(5000), allowNull: true }, { transaction });
+      await queryInterface.addColumn("dental_clinics", "realNameCampaign", { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false }, { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
