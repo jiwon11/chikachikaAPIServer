a620a7055274a1dadea28098c81058bf6cabbf55
jiwon11 <jeewonre@naver.com>
2021-01-14 19:09:05 +0900

    리뷰글 상세보기 댓글 개수 값 추가

--- utils/Class/review.js
+++ utils/Class/review.js
@@ -8,7 +8,15 @@ module.exports.getOne = async function (db, reviewId) {
       },
     },
     attributes: {
-      include: [[Sequelize.literal(`(SELECT TIMESTAMPDIFF(SECOND,review.updatedAt,NOW()))`), "createdDiff(second)"]],
+      include: [
+        [Sequelize.literal(`(SELECT TIMESTAMPDIFF(SECOND,review.updatedAt,NOW()))`), "createdDiff(second)"],
+        [
+          Sequelize.literal(
+            "(SELECT COUNT(*) FROM review_comments WHERE review_comments.reviewId = review.id AND deletedAt IS null) + (SELECT COUNT(*) FROM Review_reply LEFT JOIN review_comments ON (review_comments.id = Review_reply.commentId) WHERE review_comments.reviewId = review.id)"
+          ),
+          "reviewCommentsNum",
+        ],
+      ],
     },
     include: [
       {
