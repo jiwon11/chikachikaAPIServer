2f1d461b1c07770a734e499e2343eee1721fa5ec
jiwon11 <jeewonre@naver.com>
2021-03-09 15:59:11 +0900

    jest 유닛 테스트 & api 호출

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,3 +1,15 @@
-test("1 + 1 은 2일겁니다.", () => {
-  expect(1 + 1).toEqual(2);
+const axios = require("axios");
+const API_ENDPOINT = "https://43tbm5rlw1.execute-api.ap-northeast-1.amazonaws.com/test";
+axios.defaults.adapter = require("axios/lib/adapters/http");
+
+test("GET search Treatments", async () => {
+  const res = await axios
+    .get(`${API_ENDPOINT}/search/treatments?q=${encodeURIComponent("충치")}`, {
+      headers: {
+        Authorization:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YjhmZTUwLTdmYmQtMTFlYi1iZGRiLTMxODE5ZTA1YzZlNSIsImlhdCI6MTYxNTE4NDQ5NSwiZXhwIjoxNjQ2NzQyMDk1fQ.m8G3Odh1zXxfxyoEytpuI7q9eS1L7SxGgV8lOQvdmE4",
+      },
+    })
+    .then((response) => response);
+  expect(res.status).toBe(200);
 });
