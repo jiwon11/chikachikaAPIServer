b6b59d0b9071624c0afaaf2c661feaf350084f44
Jiwon <jeewonre@naver.com>
2021-04-07 17:01:52 +0900

    영수증 리뷰 검수 API 수정

--- services/admin.js
+++ services/admin.js
@@ -20,50 +20,46 @@ module.exports.verifyBills = async function verifyBills(event) {
         },
       ],
     });
-    if (review.certifiedBill === false) {
+    var message;
+    console.log(JSON.stringify(review.user));
+    if (purpose === "permission") {
       await review.update({
         certifiedBill: true,
       });
-      var message;
-      console.log(JSON.stringify(review.user));
-      if (purpose === "permission") {
-        message = {
-          notification: {
-            title: "영수증 검수가 완료되었습니다.",
-            body: "영수증 검수 결과, 리뷰의 영수증 인증이 확인되었습니다.",
-          },
-          data: { targetId: `${review.id}` },
-          token: review.user.fcmToken,
-        };
-      } else {
-        message = {
-          notification: {
-            title: "영수증 검수가 완료되었습니다.",
-            body: "영수증 검수 결과, 반려되어 리뷰의 영수증 인증이 보류되었습니다.",
-          },
-          data: { targetId: `${review.id}` },
-          token: review.user.fcmToken,
-        };
-      }
-      const userNotifyConfig = await db.NotificationConfig.findOne({
-        where: {
-          userId: review.user.id,
+      message = {
+        notification: {
+          title: "영수증 검수가 완료되었습니다.",
+          body: "영수증 검수 결과, 리뷰의 영수증 인증이 확인되었습니다.",
         },
-      });
-      if (userNotifyConfig.event === true) {
-        const fcmResponse = await pushFcm(message);
-        console.log(fcmResponse);
-      }
-      return {
-        statusCode: 200,
-        body: `{ status: "OK" }`,
+        data: { targetId: `${review.id}` },
+        token: review.user.fcmToken,
       };
     } else {
-      return {
-        statusCode: 400,
-        body: `{ status: "Error", message: "이미 승인된 리뷰입니다."}`,
+      await review.update({
+        certifiedBill: false,
+      });
+      message = {
+        notification: {
+          title: "영수증 검수가 완료되었습니다.",
+          body: "영수증 검수 결과, 반려되어 리뷰의 영수증 인증이 보류되었습니다.",
+        },
+        data: { targetId: `${review.id}` },
+        token: review.user.fcmToken,
       };
     }
+    const userNotifyConfig = await db.NotificationConfig.findOne({
+      where: {
+        userId: review.user.id,
+      },
+    });
+    if (userNotifyConfig.event === true) {
+      const fcmResponse = await pushFcm(message);
+      console.log(fcmResponse);
+    }
+    return {
+      statusCode: 200,
+      body: `{ status: "OK" }`,
+    };
   } catch (error) {
     console.error(error);
     return {
