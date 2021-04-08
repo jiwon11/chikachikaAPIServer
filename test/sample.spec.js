b9b16a46a1df1afe8616a3b52268754002125638
Jiwon <jeewonre@naver.com>
2021-04-08 14:27:45 +0900

    prod stage 테스트 예외처리

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -7,30 +7,32 @@ const stage = stageArg ? stageArg.split("=")[1] : "dev";
 console.log("stage:", stage);
 var USERTOKEN;
 var API_ENDPOINT;
+var STATUS;
 if (stage === "test") {
   API_ENDPOINT = "https://test-api.chikachika-app.com";
   USERTOKEN = process.env.USERTOKEN;
+  STATUS = 200;
 } else if (stage === "dev") {
   API_ENDPOINT = "https://dev-api.chikachika-app.com";
   USERTOKEN = process.env.USERTOKEN;
+  STATUS = 200;
 } else if (stage === "prod") {
   API_ENDPOINT = "https://api.chikachika-app.com";
   USERTOKEN = process.env.USERTOKEN;
+  STATUS = 401;
 }
 
-if (stage !== "prod") {
-  axios.defaults.adapter = require("axios/lib/adapters/http");
-  describe("GET search", () => {
-    test("GET search keyWaord Search", async () => {
-      const res = await axios
-        .get(`${API_ENDPOINT}/search/review?query=${encodeURIComponent("인천")}&lat=37.29404121591071&long=127.04600809585094&type=All&limit=10&offset=0&order=createdAt&region=all&cityId=-1`, {
-          headers: {
-            Authorization: USERTOKEN,
-          },
-        })
-        .then((response) => response);
-      console.log(res);
-      expect(res.status).toBe(200);
-    });
+axios.defaults.adapter = require("axios/lib/adapters/http");
+describe("GET search", () => {
+  test("GET search keyWord Search", async () => {
+    const res = await axios
+      .get(`${API_ENDPOINT}/search/review?query=${encodeURIComponent("인천")}&lat=37.29404121591071&long=127.04600809585094&type=All&limit=10&offset=0&order=createdAt&region=all&cityId=-1`, {
+        headers: {
+          Authorization: USERTOKEN,
+        },
+      })
+      .then((response) => response);
+    console.log(res);
+    expect(res.status).toBe(401);
   });
-}
+});
