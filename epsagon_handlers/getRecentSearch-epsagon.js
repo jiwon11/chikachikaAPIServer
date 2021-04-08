3553b39ef0d96cbf9c150fac2738ca8aa0de4844
Jiwon <jeewonre@naver.com>
2021-04-08 18:41:39 +0900

    커뮤니티 동네 추가 로직 수정

---
+++ epsagon_handlers/getRecentSearch-epsagon.js
@@ -0,0 +1,23 @@
+
+const epsagon = require('epsagon');
+const epsagonHandler = require('../services/recentSearch.js');
+
+
+
+  if (!process.env.EPSAGON_IGNORED_KEYS) {
+    process.env.EPSAGON_IGNORED_KEYS = "";
+  }
+
+  if (!process.env.EPSAGON_URLS_TO_IGNORE) {
+    process.env.EPSAGON_URLS_TO_IGNORE = "";
+  }
+
+epsagon.init({
+    token: 'ecaf8eeb-6e6b-4114-80b3-0d9014bbc0f7',
+    appName: 'chikachika-dev',
+    traceCollectorURL: undefined,
+    metadataOnly: Boolean(0),
+    labels: [],
+});
+
+exports.getRecent = epsagon.lambdaWrapper(epsagonHandler.getRecent);
